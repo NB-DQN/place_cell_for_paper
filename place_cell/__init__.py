b5eef6ffda339079282120317b50c9869c3ae045
iTakeshi <takeshi.ito.doraemon@gmail.com>
2016-01-05 15:50:14 +0900

    remove unnecessary imports

--- place_cell/__init__.py
+++
@@ -1,4 +0,0 @@
-from place_cell                  import PlaceCell
-from deterministic_place_cell    import DeterministicPlaceCell
-from path_integrating_place_cell import PathIntegratingPlaceCell
-from visual_place_cell           import VisualPlaceCell
